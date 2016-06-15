2215d6c9c5e8819c4bc40211295937120b656cc1
540513310 <15899685505@163.com>
2016-06-16 02:07:28 +0800

    dialog方法注册表单

--- js/index.js
+++ js/index.js
@@ -3,5 +3,16 @@
 		icons:{
 			primary:'ui-icon-search',
 		},
+		size:{
+
+		},
+	});
+
+	$("#reg").dialog({
+		autoOpen:false,
+	});
+
+	$("#reg_a").click(function () {
+		$("#reg").dialog('open');
 	});
 })
