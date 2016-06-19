0c1b4644d0d889ee2d23717c5e8dd61591979de1
540513310 <15899685505@163.com>
2016-06-19 22:09:01 +0800

    邮箱自动补齐

--- js/index.js
+++ js/index.js
@@ -20,6 +20,46 @@
 	});
 
 	$('#reg').buttonset();
-	$('#reg input[title]').tooltip();
+	$('#reg input[title]').tooltip({
+		position:{
+			my:'left',
+			at:'right'
+		}
+
+		});
 	$('#birth').datepicker();
+
+	$('#email').autocomplete({
+		delay:0,
+		source:function (request,response) {
+			var hosts=['qq.com','163.com','gmail.com','sina.com.cn','hostmail.com'],
+			userTypeIn=request.term,			//用户输入的内容
+			name=userTypeIn,					//暂存用户输入的内容
+			
+			host=''	,							//邮箱的域名
+			result=[],							//最终的邮箱列表
+			ix=userTypeIn.indexOf('@');			//输入的@所在的位置，如果输入中包含@则得到@所在的索引，否则得到-1
+			result.push(userTypeIn);
+			//当有@时，重新分配用户名和域名
+			if(ix>-1){
+				 name=userTypeIn.slice(0,ix);	//@符号之前的为邮箱中的用户名
+				 host=userTypeIn.slice(ix+1);	//@符号之后的为邮箱中的域名
+			}
+			//从hosts数组中过滤出用户输入的域名的元素
+			//如果用户已经输入了域名中的一部分，则从hosts中过滤出，否则是全部的hosts
+			if(name)
+			{
+				var host_grep=(host?$.grep(hosts,function (value,index) {	
+				return value.indexOf(host)>-1;
+			    }):hosts);
+			    //把过滤出来的域名和用户名组合成邮箱列表
+			    var host_list=$.map(host_grep,function (value,index) {
+			    	return name+'@'+value;
+			    });
+			    result=result.concat(host_list);
+			}
+			
+			response(result);
+		},
+	});
 })
